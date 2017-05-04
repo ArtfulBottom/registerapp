ff021de2c453c3a714b1e1c986282c6577e0e095
Heath Richardson <mheathr@gmail.com>
2017-05-03 21:24:00 -0500

    added added putTransaction and putTransactionEntry methods in TransactionEntryService and TransactionService

--- app/src/main/java/edu/uark/uarkregisterapp/models/api/services/TransactionEntryService.java
+++ app/src/main/java/edu/uark/uarkregisterapp/models/api/services/TransactionEntryService.java
@@ -1,4 +1,23 @@
 package edu.uark.uarkregisterapp.models.api.services;
 
-public class TransactionEntryService {
+import org.json.JSONObject;
+
+import edu.uark.uarkregisterapp.models.api.TransactionEntry;
+import edu.uark.uarkregisterapp.models.api.enums.ApiLevel;
+import edu.uark.uarkregisterapp.models.api.enums.TransactionApiRequestStatus;
+import edu.uark.uarkregisterapp.models.api.enums.TransactionEntryApiMethod;
+import edu.uark.uarkregisterapp.models.api.interfaces.PathElementInterface;
+
+public class TransactionEntryService extends BaseRemoteService {
+    public TransactionEntry putTransactionEntry(TransactionEntry entry) {
+        JSONObject rawJsonObject = this.putSingle(
+                (new PathElementInterface[]{TransactionEntryApiMethod.TRANSACTION, ApiLevel.ONE}), entry.convertToJson()
+        );
+
+        if (rawJsonObject != null) {
+            return (new TransactionEntry()).loadFromJson(rawJsonObject);
+        } else {
+            return new TransactionEntry().setApiRequestStatus(TransactionApiRequestStatus.UNKNOWN_ERROR);
+        }
+    }
 }
--- app/src/main/java/edu/uark/uarkregisterapp/models/api/services/TransactionService.java
+++ app/src/main/java/edu/uark/uarkregisterapp/models/api/services/TransactionService.java
@@ -1,4 +1,23 @@
 package edu.uark.uarkregisterapp.models.api.services;
 
-public class TransactionService {
+import org.json.JSONObject;
+
+import edu.uark.uarkregisterapp.models.api.Transaction;
+import edu.uark.uarkregisterapp.models.api.enums.ApiLevel;
+import edu.uark.uarkregisterapp.models.api.enums.TransactionApiMethod;
+import edu.uark.uarkregisterapp.models.api.enums.TransactionApiRequestStatus;
+import edu.uark.uarkregisterapp.models.api.interfaces.PathElementInterface;
+
+public class TransactionService extends BaseRemoteService {
+    public Transaction putTransaction(Transaction transaction) {
+        JSONObject rawJsonObject = this.putSingle(
+                (new PathElementInterface[]{TransactionApiMethod.TRANSACTION, ApiLevel.ONE}), transaction.convertToJson()
+        );
+
+        if (rawJsonObject != null) {
+            return (new Transaction()).loadFromJson(rawJsonObject);
+        } else {
+            return new Transaction().setApiRequestStatus(TransactionApiRequestStatus.UNKNOWN_ERROR);
+        }
+    }
 }
